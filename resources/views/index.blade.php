f98cc4ffabf052fda5c4fcf003f5075e9b400b55
Ridwan Ridlo Nugroho <9uan1ingu@gmail.com>
2022-06-28 20:12:20 +0700

    update landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,31 +3,40 @@
 @section("main")
 
 <div class="wrapper">
-  <div class="page-header clear-filter" filter-color="orange">
+  <div class="page-header clear-filter" filter-color="orange" style="overflow: unset">
     <div class="page-header-image" data-parallax="true" style="background-image: url('https://drive.google.com/uc?export=view&id=1j4OQpis-dpF5efjQ9qaQfsclTIpTC4Wr'); filter : brightness(50%);"></div>
     <div class="container d-flex align-items-center">
-      <div class="d-flex" style="text-align: start!important; max-width: 25vw!important;">
+      <div class="d-flex" style="text-align: start!important; max-width: 40vw!important;">
         <p>Belajar Riset dan Menulis Karya Ilmiah dengan Mudah serta Menyenangkan Membangun Ekosistem Keilmiahan dan Penelitian Untuk Mencetak Inovator Muda Indonesia</p>
+      </div>
+      <div class="position-relative" style="left: 8vw; top: 13vh;">
+        <img style="width: 20vw; border-radius: 20px;" class="img-raised" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
       </div>
     </div>
   </div>
 </div>
-<div class="position-absolute" style="right: 20vw; z-index:1001; top: 25vh;">
-  <img style="width: 20vw; border-radius: 10px;" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
-</div>
 <main class="container mt-5">
   <h1 style="margin-top: 135px!important; margin-bottom: 60px;!important" class="text-center">
-    Rumah Kompetisi Class
+    <strong>Rumah Kompetisi Class</strong>
   </h1>
-  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="row row-cols-md-3 g-4">
     <div class="col-md-4">
       <div class="card overflow-hidden" style="max-width: 20rem; border-radius: 20px; box-shadow: black">
-        <div class="card-header bg-transparent">
-          <i class="fa-solid fa-tags d-inline"></i>
-          <p class="d-inline">Diskon 80 %</p>
+        <img class="card-img-top img-raised" src="https://drive.google.com/uc?export=view&id=1j4OQpis-dpF5efjQ9qaQfsclTIpTC4Wr" alt="Card image cap" style="border-radius: 20px; filter: brightness(40%)">
+        <div class="text-white card-img-overlay d-flex overflow-hidden d-flex align-items-center" style="max-height: 48%">
+          <div style="min-width: 60%">
+            <i class="fa-solid fa-tags d-inline"></i>
+            <p class="d-inline">Diskon 80 %</p>
+            <h3 class="m-0">Essay Class</h3>
+            <h5>1 Bulan</h5>
+            <h4><s>Rp 150.000 ,-</s></h4>
+          </div>
+          <div>
+            <img style = "border-radius: 1rem;" src="https://lppm.unnes.ac.id/storage/2015/08/Logo-Transparan-Warna-1.png" alt="">
+          </div>
         </div>
-        <div class="card-body text-success">
-          <h5 class="card-title">Success card title</h5>
+        <div class="card-body">
+          <h5 class="card-title text-center"><strong>Rp. 50.000 ,-</strong></h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         <div class="card-footer bg-transparent pl-3 pr-3">
@@ -39,14 +48,284 @@
       </div>
     </div>
     <div class="col-md-4">
-      <div class="card overflow-hidden" style="max-width: 20rem; border-radius: 10px;">
+      <div class="card overflow-hidden" style="max-width: 20rem; border-radius: 20px; box-shadow: black">
+        <img class="card-img-top img-raised" src="https://drive.google.com/uc?export=view&id=1j4OQpis-dpF5efjQ9qaQfsclTIpTC4Wr" alt="Card image cap" style="border-radius: 20px; filter: brightness(40%)">
+        <div class="text-white card-img-overlay d-flex overflow-hidden d-flex align-items-center" style="max-height: 48%">
+          <div style="min-width: 60%">
+            <i class="fa-solid fa-tags d-inline"></i>
+            <p class="d-inline">Diskon 80 %</p>
+            <h3 class="m-0">Essay Class</h3>
+            <h5>1 Bulan</h5>
+            <h4><s>Rp 150.000 ,-</s></h4>
+          </div>
+          <div>
+            <img style = "border-radius: 1rem;" src="https://agricia.faperta.ugm.ac.id/wp-content/uploads/sites/377/2018/06/logo-ugm-png.png" alt="">
+          </div>
+        </div>
         <div class="card-body">
-          <h5 class="card-title text-center bold"><strong>English Class</strong></h5>
-          <p class="card-text">Ini deskripsi</p>
+          <h5 class="card-title text-center"><strong>Rp. 50.000 ,-</strong></h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="card-footer bg-transparent pl-3 pr-3">
           <div class="d-flex justify-content-between align-items-center">
             <a href="#">more</a>
             <a href="https://wa.me/085900221521" class="btn btn-primary">Beli</a>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card overflow-hidden" style="max-width: 20rem; border-radius: 20px; box-shadow: black">
+        <img class="card-img-top img-raised" src="https://drive.google.com/uc?export=view&id=1j4OQpis-dpF5efjQ9qaQfsclTIpTC4Wr" alt="Card image cap" style="border-radius: 20px; filter: brightness(40%)">
+        <div class="text-white card-img-overlay d-flex overflow-hidden d-flex align-items-center" style="max-height: 48%">
+          <div style="min-width: 60%">
+            <i class="fa-solid fa-tags d-inline"></i>
+            <p class="d-inline">Diskon 80 %</p>
+            <h3 class="m-0">Essay Class</h3>
+            <h5>1 Bulan</h5>
+            <h4><s>Rp 150.000 ,-</s></h4>
+          </div>
+          <div>
+            <img style = "border-radius: 1rem;" src="https://agricia.faperta.ugm.ac.id/wp-content/uploads/sites/377/2018/06/logo-ugm-png.png" alt="">
+          </div>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title text-center"><strong>Rp. 50.000 ,-</strong></h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="card-footer bg-transparent pl-3 pr-3">
+          <div class="d-flex justify-content-between align-items-center">
+            <a href="#">more</a>
+            <a href="https://wa.me/085900221521" class="btn btn-primary">Beli</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<main class="mt-5 text-white p-5" style="background-image: url('/img/header.jpg'); height: 400px; background-position: center">
+  <div class="container d-flex align-items-center" style="height: 100%">
+    <div style="max-width: 60%">
+      <h2><strong class="text-primary">Rumah Kompetisi</strong> Platinum Class</h2>
+      <h4>Pelayanan dan Fasilitas Bagi Para Konsumen yang Ingin Berkonsultasi secara Privat dengan para tentor mengenai persiapan lomba</h4>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg"><strong>Spesial Promo</strong></button>
+
+      <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content p-4 text-black">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <h1 class="text-center"><strong>Promo Platinum</strong></h1>
+            <div class="row row-cols-md-3 g-4">
+              <div class="col-md-4">
+                <div class="card overflow-hidden" style="max-width: 20rem; border-radius: 20px; box-shadow: black">
+                  <img class="card-img-top img-raised" src="https://drive.google.com/uc?export=view&id=1j4OQpis-dpF5efjQ9qaQfsclTIpTC4Wr" alt="Card image cap" style="border-radius: 20px; filter: brightness(40%)">
+                  <div class="text-white card-img-overlay d-flex overflow-hidden d-flex align-items-center" style="max-height: 48%">
+                    <div style="min-width: 60%">
+                      <i class="fa-solid fa-tags d-inline"></i>
+                      <p class="d-inline">Diskon 80 %</p>
+                      <h3 class="m-0">Essay Class</h3>
+                      <h5>1 Bulan</h5>
+                      <h4><s>Rp 150.000 ,-</s></h4>
+                    </div>
+                    <div>
+                      <img style = "border-radius: 1rem;" src="https://lppm.unnes.ac.id/storage/2015/08/Logo-Transparan-Warna-1.png" alt="">
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title text-center"><strong>Rp. 50.000 ,-</strong></h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                  <div class="card-footer bg-transparent pl-3 pr-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                      <a href="#">more</a>
+                      <a href="https://wa.me/085900221521" class="btn btn-primary">Beli</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="position-relative" style="left: 5vw; bottom: 10vh;">
+      <img style="width: 20vw; border-radius: 20px;" class="img-raised" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+    </div>
+  </div>
+</main>
+<main class="container mt-5">
+  <div class="row">
+    <div class="col-lg-7">
+      <h3 class="text-primary">Rumah Kompetisi</h3>
+      <h1><strong>Mengapa Kami ?</strong></h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit sint, quo magni sit inventore et necessitatibus delectus similique laudantium molestias modi voluptate tempore mollitia dolore, obcaecati voluptas, assumenda accusantium esse.</p>
+    </div>
+    <div class="col-lg-5">
+      <div class="row mb-2">
+        <div class="col-sm-2">
+          <div class="d-flex bg-primary rounded-circle text-white" style="width: 40px; height:40px;">
+            <i class="fa-solid fa-book-bookmark m-auto"></i>
+          </div>
+        </div>
+        <div class="col">
+          <h6>Terpercaya</h6>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, voluptatum.</p>
+        </div>
+      </div>
+      <div class="row  mb-2">
+        <div class="col-sm-2">
+          <div class="d-flex bg-primary rounded-circle text-white" style="width: 40px; height:40px;">
+            <i class="fa-solid fa-clock m-auto"></i>
+          </div>
+        </div>
+        <div class="col">
+          <h6>Terpercaya</h6>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, voluptatum.</p>
+        </div>
+      </div>
+      <div class="row  mb-2">
+        <div class="col-sm-2">
+          <div class="d-flex bg-primary rounded-circle text-white" style="width: 40px; height:40px;">
+            <i class="fa-solid fa-user-tie m-auto"></i>
+          </div>
+        </div>
+        <div class="col">
+          <h6>Terpercaya</h6>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, voluptatum.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<main class="container mt-5">
+  <div class="container">
+    <div class="row">
+      <div class="MultiCarousel" data-items="1,2,3,3" data-slide="1" id="MultiCarousel"  data-interval="1000">
+              <div class="MultiCarousel-inner">
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="position-relative d-flex text-white">
+                      <div class="filter filter-dark">
+                        <img style="height : 250px; object-fit: cover" src="https://drive.google.com/uc?export=view&id=1l1qCGKEVl6QyrueHDiIxzP54gpKxPmWQ" alt="">
+                      </div>
+                      <div class="position-absolute text-center" style="left: 50%; bottom: 0; transform: translate(-50%, 0);">
+                        <h4><strong>Nama Saya</strong></h4>
+                        <p>adalah mentor ternama</p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <button class="btn btn-primary leftLst btn-icon"><i class="fa-solid fa-arrow-left"></i></button>
+              <button class="btn btn-primary rightLst btn-icon"><i class="fa-solid fa-arrow-right"></i></button>
+          </div>
+    </div>
+  </div>
+
+  <h1 class="mt-5 mb-5 text-center"><strong>Q n A</strong></h1>
+</main>
+<main class="mt-5 p-5" style="background-color:rgba(128, 128, 128, 0.3);">
+  <div class="container overflow-hidden pt-4" style="background-color: white; border-radius : 20px;">
+    <div>
+      <p>
+        <a style="width: 100%" class="btn btn-primary btn-round" data-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">
+          tanya 1
+        </a>
+      </p>
+      <div class="collapse" id="collapseExample1">
+        <div class="card card-body">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </div>
+    </div>
+    <div>
+      <p>
+        <a style="width: 100%" class="btn btn-primary btn-round" data-toggle="collapse" href="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
+          tanya 2
+        </a>
+      </p>
+      <div class="collapse" id="collapseExample2">
+        <div class="card card-body">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
         </div>
       </div>
     </div>
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,7 @@
 <link rel="icon" type="image/png" href="/img/favicon.png">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <title>
-    Now UI Kit by Creative Tim
+    Rumah Kompetisi
 </title>
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 <!--     Fonts and icons     -->
@@ -23,12 +23,44 @@
         min-height: 0;
         height: 400px!important;
     }
+    .MultiCarousel {
+        float: left;
+        overflow: hidden;
+        padding: 15px;
+        width: 100%;
+        position:relative;
+    }
+    .MultiCarousel .MultiCarousel-inner {
+        transition: 1s ease all;
+        float: left;
+    }
+    .MultiCarousel .MultiCarousel-inner .item { float: left;}
+    .MultiCarousel .MultiCarousel-inner .item > div { text-align: center; margin:10px; color:#666;}
+    .MultiCarousel .leftLst, .MultiCarousel .rightLst { position:absolute; border-radius:50%;top:calc(50% - 20px); }
+    .MultiCarousel .leftLst { left:0; }
+    .MultiCarousel .rightLst { right:0; }
+    
+    .MultiCarousel .leftLst.over, .MultiCarousel .rightLst.over { pointer-events: none; background:#ccc; }
+
+    .filter:after {
+        display: block;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        position: absolute;
+        content: '';
+        opacity: 0.9;
+    }
+    .filter-dark:after {
+        background-image: linear-gradient(to bottom, rgba(5,5,5,0), rgba(5,5,5,1));
+    }
 </style>
 </head>
 
 <body class="index-page sidebar-collapse">
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="75">
+<nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="50">
     <div class="container">
     <div class="navbar-translate">
         <a class="navbar-brand" href="/" rel="tooltip" title="Belajar Riset dan Menulis Karya Ilmiah dengan Mudah serta Menyenangkan" data-placement="bottom">
@@ -149,6 +181,114 @@ function scrollToDownload() {
     }, 1000);
     }
 }
+</script>
+<script>
+    $(document).ready(function () {
+    var itemsMainDiv = ('.MultiCarousel');
+    var itemsDiv = ('.MultiCarousel-inner');
+    var itemWidth = "";
+
+    $('.leftLst, .rightLst').click(function () {
+        var condition = $(this).hasClass("leftLst");
+        if (condition)
+            click(0, this);
+        else
+            click(1, this)
+    });
+
+    ResCarouselSize();
+
+
+
+
+    $(window).resize(function () {
+        ResCarouselSize();
+    });
+
+    //this function define the size of the items
+    function ResCarouselSize() {
+        var incno = 0;
+        var dataItems = ("data-items");
+        var itemClass = ('.item');
+        var id = 0;
+        var btnParentSb = '';
+        var itemsSplit = '';
+        var sampwidth = $(itemsMainDiv).width();
+        var bodyWidth = $('body').width();
+        $(itemsDiv).each(function () {
+            id = id + 1;
+            var itemNumbers = $(this).find(itemClass).length;
+            btnParentSb = $(this).parent().attr(dataItems);
+            itemsSplit = btnParentSb.split(',');
+            $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+            if (bodyWidth >= 1200) {
+                incno = itemsSplit[3];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 992) {
+                incno = itemsSplit[2];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 768) {
+                incno = itemsSplit[1];
+                itemWidth = sampwidth / incno;
+            }
+            else {
+                incno = itemsSplit[0];
+                itemWidth = sampwidth / incno;
+            }
+            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+            $(this).find(itemClass).each(function () {
+                $(this).outerWidth(itemWidth);
+            });
+
+            $(".leftLst").addClass("over");
+            $(".rightLst").removeClass("over");
+
+        });
+    }
+
+
+    //this function used to move the items
+    function ResCarousel(e, el, s) {
+        var leftBtn = ('.leftLst');
+        var rightBtn = ('.rightLst');
+        var translateXval = '';
+        var divStyle = $(el + ' ' + itemsDiv).css('transform');
+        var values = divStyle.match(/-?[\d\.]+/g);
+        var xds = Math.abs(values[4]);
+        if (e == 0) {
+            translateXval = parseInt(xds) - parseInt(itemWidth * s);
+            $(el + ' ' + rightBtn).removeClass("over");
+
+            if (translateXval <= itemWidth / 2) {
+                translateXval = 0;
+                $(el + ' ' + leftBtn).addClass("over");
+            }
+        }
+        else if (e == 1) {
+            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+            translateXval = parseInt(xds) + parseInt(itemWidth * s);
+            $(el + ' ' + leftBtn).removeClass("over");
+
+            if (translateXval >= itemsCondition - itemWidth / 2) {
+                translateXval = itemsCondition;
+                $(el + ' ' + rightBtn).addClass("over");
+            }
+        }
+        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+    }
+
+    //It is used to get some elements from btn
+    function click(ell, ee) {
+        var Parent = "#" + $(ee).parent().attr("id");
+        var slide = $(Parent).attr("data-slide");
+        ResCarousel(ell, Parent, slide);
+    }
+
+});
 </script>
 </body>
 
