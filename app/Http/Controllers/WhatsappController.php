e8f1214d37762b9d12dc342814c4eca11e352176
Ridwan Ridlo Nugroho <9uan1ingu@gmail.com>
2022-07-02 05:30:53 +0700

    tes

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
+use App\Models\Kelas;
+use App\Models\Platinum;
 
 class WhatsappController extends Controller
 {
     public function whatsapp(Request $request) {
+        $kelas = Kelas::get();
+        $platinum = Platinum::get();
         $data = $request->all();
         $number = $data["nomor"];
         $message = $data["pesan"];
@@ -18,6 +22,29 @@ class WhatsappController extends Controller
 				"number" => $number,
 				"message" => "halo juga"
 			]);
+        } else if (preg_match("/kelas/i", $message)) {
+            Http::post("https://wa-pkm.herokuapp.com/sendlist", [
+                'number' => $number,
+                'body' => 'List Kelas',
+                'title' => 'List Kelas',
+                'message' => 
+                    [
+                        [
+                            'title' => 'ini pesan',
+                            'rows' => 
+                                [
+                                        [
+                                            'title' => 'produk 1',
+                                            'description' => 'ini adalah list produk 1',
+                                        ],
+                                        [
+                                            'title' => 'produk 1',
+                                            'description' => 'ini adalah list produk 1',
+                                        ]
+                                ],
+                        ],
+                    ],
+                ]);
         }
     }
 }
