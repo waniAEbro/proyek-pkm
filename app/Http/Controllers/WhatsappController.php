0eb4051df00d28eec6aa1fe1477fac309e8e5e20
Ridwan Ridlo Nugroho <9uan1ingu@gmail.com>
2022-07-01 22:06:43 +0700

    tes whatsapp

---
+++ app/Http/Controllers/WhatsappController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+
+class WhatsappController extends Controller
+{
+    public function whatsapp(Request $request) {
+        $data = $request->all();
+        $number = $data["number"];
+        $message = $data["message"];
+
+        if (preg_match("/halo/i", $message)) {
+            Http::post("https://wa-pkm.herokuapp.com/send", [
+				"number" => $number,
+				"message" => "halo juga"
+			]);
+        }
+    }
+}
